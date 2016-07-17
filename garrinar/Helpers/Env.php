3f32a97e662c387fee3903686e708a0948e67485
garrinar <garrinar@gmail.com>
2016-07-17 03:17:54 +0300

    Added Env helper

--- garrinar/Helpers/Env.php
+++ garrinar/Helpers/Env.php
@@ -11,11 +11,27 @@ use Illuminate\Support\Str;
  *
  *
  * @method getAppKey($default = null)
- * @method getDateFormat($default = null)
+ * @method static getDateFormat($default = null)
  *
  */
 class Env
 {
+
+    public function __call($name, $attributes)
+    {
+        if(Str::startsWith('get', $name)) {
+            $method = Str::camel($name);
+            if(method_exists(static::class, $method)) {
+                $class = static::class;
+                if(Arr::exists($attributes, 0)) {
+                    return $class::$method($attributes[0]);
+                }
+                return $class::$method();
+            }
+        }
+        return env(Str::upper(Str::snake(Str::substr($name, 3))));
+    }
+
     public static function __callStatic($name, $attributes)
     {
         if(Str::startsWith('get', $name)) {
