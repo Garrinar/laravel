9174d474dbf04ea1bb8dcb8ebb4bbd5ab8bfea65
garrinar <garrinar@gmail.com>
2016-07-17 03:21:32 +0300

    Added Env helper

--- garrinar/Helpers/Env.php
+++ garrinar/Helpers/Env.php
@@ -19,17 +19,7 @@ class Env
 
     public function __call($name, $attributes)
     {
-        if(Str::startsWith('get', $name)) {
-            $method = Str::camel($name);
-            if(method_exists(static::class, $method)) {
-                $class = static::class;
-                if(Arr::exists($attributes, 0)) {
-                    return $class::$method($attributes[0]);
-                }
-                return $class::$method();
-            }
-        }
-        return env(Str::upper(Str::snake(Str::substr($name, 3))));
+        return static::__callStatic($name, $attributes);
     }
 
     public static function __callStatic($name, $attributes)
